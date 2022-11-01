e9ecd8c0e70f1e5e7670c44fccedabef7c04e2ce
usoobin <112929005+usoobin@users.noreply.github.com>
2022-11-01 16:56:28 +0900

    04

--- main.c
+++ main.c
@@ -1,31 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5
+#define SIZE 4
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+void square_array(int a[], int size);
+void print_array(int a[], int size);
+
 int main(int argc, char *argv[]) 
 {
-	int i;
-	int a[SIZE] = {1,2,3,4,5};
-	int b[SIZE] = {1,2,3,4,5};
-	int flag = 1;  //이벤트가 발생하면 1 아니면 0 
+	int list[SIZE] = {1,2,3,4};
 
-	for (i=0; i<SIZE; i++)
-	{
-		if (a[i] != b[i])
-		{
-			flag = 1; 
-			printf("배열값이 같은 값을 가집니다\n");
-			break; // 5개중 같은게 없을경우 나감 
-		}
-		
-	}
-	if(flag == 0 )
-	{
-		printf("배열값이 같은 값을 가집니다\n");
-	}
-
+	print_array(list, SIZE);
+	square_array(list, SIZE);
+	print_array(list, SIZE);
 	
 	return 0;
 }
+
+	void square_array(int a[], int size)
+	{
+		int i;
+
+		for(i=0; i<SIZE; i++)
+		{ 
+			a[i] = a[i] * a[i];
+		}
+	}
+
+	void print_array(int a[], int size)
+	{			
+		int i;
+
+		for(i=0; i<SIZE; i++)
+		{
+			printf("%3d", a[i]);
+		}
+		
+		printf("\n");
+	}
